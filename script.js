ebd9dc4ee2a85f5d69596126245d20af175aacd5
Nisha Bukke <sononenisha827@gmail.com>
2023-11-08 18:49:21 +0530

    my-portfolio

---
+++ script.js
@@ -0,0 +1,28 @@
+$('.nav ul li a').click(function(e) {
+  e.preventDefault();
+  $('.nav ul li a').removeClass('active');
+  $(this).addClass('active');
+});
+
+function activeTab(evt, id) {
+           
+  // Get all elements with class="tablinks" and remove the class "active"
+   let tabactive = document.getElementsByClassName("TabButtonSelected");
+   tabactive[0].className = tabactive[0].className.replace(" TabButtonSelected", "");
+
+   document.getElementById(id).style.display = "block";
+   evt.currentTarget.className += " TabButtonSelected";
+
+   displaySection(evt,id)
+}
+
+function displaySection(evt, id) {
+
+   let tabactive = document.getElementsByClassName("tab-section");
+   tabactive[0].className = tabactive[0].className.replace(" d-chart-show", "d-chart-n");
+   // add below line of codes
+   [...document.querySelectorAll('div.tab-section')].forEach(item => item.style.display='none')
+   document.getElementById("Section" + id).style.display = "block";
+   evt.currentTarget.className += " d-chart-show";
+
+}
